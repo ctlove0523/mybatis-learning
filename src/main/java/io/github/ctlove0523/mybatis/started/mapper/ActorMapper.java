e7179455706a58ef0cac14434c6181e4c55e065f
ctlove0523 <478309639@qq.com>
2022-07-05 22:41:39 +0800

    支持自动生成代码

---
+++ src/main/java/io/github/ctlove0523/mybatis/started/mapper/ActorMapper.java
@@ -0,0 +1,17 @@
+package io.github.ctlove0523.mybatis.started.mapper;
+
+import io.github.ctlove0523.mybatis.started.entity.Actor;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author ctlove0523
+ * @since 2022-07-05
+ */
+public interface ActorMapper extends BaseMapper<Actor> {
+
+    Actor selectActor(int id);
+}
---
+++ src/main/java/io/github/ctlove0523/mybatis/started/mapper/AddressMapper.java
@@ -0,0 +1,16 @@
+package io.github.ctlove0523.mybatis.started.mapper;
+
+import io.github.ctlove0523.mybatis.started.entity.Address;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author ctlove0523
+ * @since 2022-07-05
+ */
+public interface AddressMapper extends BaseMapper<Address> {
+
+}
